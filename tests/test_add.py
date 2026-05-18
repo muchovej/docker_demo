7360de379299903061d27644b45ef35759be40f8
muchovej <stephen.muchovej@gmail.com>
2026-05-18 15:25:53 -0700

    -- fixed tests

--- tests/test_add.py
+++ tests/test_add.py
@@ -14,8 +14,8 @@ def test_two_negs():
     assert add(-2,-3) == -5
 
 @pytest.mark.parametrize(
-    "input1, input2, input3, expected",
-    [(2,3,5,10), (2.0,3.0, 5,10), (-2,3,1), (-2,-3,-5), (10, 2, 12)],
+    "input1, input2, expected",
+    [(2,3,5), (2.0,3.0, 5), (-2,3,1), (-2,-3,-5), (10, 2, 12)],
 )
-def test_add(input1, input2, input3, expected):
-    assert add(input1, input2, input3) == expected
+def test_add(input1, input2, expected):
+    assert add(input1, input2) == expected
